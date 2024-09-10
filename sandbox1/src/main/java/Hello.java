f5e8141376265e9f86eafaa0b36a235cd48115e6
Iryna Yakovleva <runner.on.waves@gmail.com>
2024-09-10 07:57:22 +0300

    Эксперименты с функциями

---
+++ sandbox1/src/main/java/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 - 2);
+        System.out.println(2 / 2);
+        System.out.println(((2 + 2)* 2));
+        System.out.println("Hello, " + "world!");
+        System.out.println( "2 + 2 = " + 4);
+        System.out.println("2 + 2 = " + 2 + 2);
+        System.out.println(("2 + 2 = " + (2 +2)));
+    }
+}
