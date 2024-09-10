1d91daf4ed6bc55c829a1567128a74e9d433e068
Iryna Yakovleva <runner.on.waves@gmail.com>
2024-09-10 08:20:53 +0300

    Выполнена структуризация кода

--- sandbox1/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox1/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -2,7 +2,9 @@ package ru.stqa.geometry.figures;
 
 public class Rectangle {
    public static void printRectangleArea(double a, double b) {
-        System.out.println("Площадь прямоугольника " + a + " и " + b + " = " + rectangleArea(a,b));
+       // System.out.println("Площадь прямоугольника со сторонами" + a + " и " + b + " = " + rectangleArea(a,b));
+       var text = String.format("Площадь прямоугольника со сторонами %f и % f = % f", a, b,rectangleArea(a,b));
+       System.out.println(text);
     }
 
     private static Double rectangleArea(double a, double b) {
--- sandbox1/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox1/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -1,8 +1,10 @@
 package ru.stqa.geometry.figures;
 
 public class Square {
-    public static void printSquareArea(double a) {
-        System.out.println("Площадь квадрата со стороной " + a + " = " + squareArea(a));
+    public static void printSquareArea(double side) {
+        //System.out.println("Площадь квадрата со стороной " + side + " = " + squareArea(side));
+        String text = String.format("Площадь квадрата со стороной %f = %f", side, squareArea(side));
+        System.out.println(text);
     }
 
     private static double squareArea(double a) {
