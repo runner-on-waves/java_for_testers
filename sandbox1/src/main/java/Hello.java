ae2d323fbb557b28524a39e39eab6328b260e172
Iryna Yakovleva <runner.on.waves@gmail.com>
2024-09-14 08:14:53 +0300

    Лекция 2.1  примеры по исключениям

--- sandbox1/src/main/java/Hello.java
+++ sandbox1/src/main/java/Hello.java
@@ -2,22 +2,28 @@ import java.io.File;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        var configFile = new File("sandbox1/build.gradle");
+        try {
+            var z = calculate();
+            System.out.println(z);
+            System.out.println("Hello, world!");
+        } catch (ArithmeticException exception) {
+            //System.out.println(exception.getMessage());
+            exception.printStackTrace();
+        }
+        /* var configFile = new File("sandbox1/build.gradle");
         System.out.println(configFile.exists());
-        System.out.println(configFile.getAbsolutePath());
-        /*
-        System.out.println(2 + 2);
-        System.out.println(2 * 2);
-        System.out.println(2 - 2);
-        System.out.println(2 / 2);
-        System.out.println(((2 + 2)* 2));
-        System.out.println("Hello, " + "world!");
-        System.out.println( "2 + 2 = " + 4);
-        System.out.println("2 + 2 = " + 2 + 2);
-        System.out.println(("2 + 2 = " + (2 +2)));
-        */
+        System.out.println(configFile.getAbsolutePath());*/
+    }
 
+    private static int calculate() {
+        var x = 1;
+        var y = 1;
+        var z = divide(x, y);
+        return z;
+    }
+
+    private static int divide(int x, int y) {
+        var z = x / y;
+        return z;
     }
 }
