36c86169ba58bcc626b5903374637cca9263f667
Iryna Yakovleva <runner.on.waves@gmail.com>
2024-09-21 16:14:43 +0300

    Лекция 3.4  Классы помощники . Добавлен класс GroupHelper

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -6,22 +6,17 @@ import org.junit.jupiter.api.Test;
 public class GroupCreationTests extends TestBase {
     @Test
     public void canCreateGroup() {
-        app.openGroupsPage();
-        app.createGroup(new GroupData("new name", "new header", "new footer"));
+        app.groups().createGroup(new GroupData("new name", "new header", "new footer"));
     }
 
     @Test
     public void canCreateGroupWithEmptyName() {
-        app.openGroupsPage();
-        app.createGroup(new GroupData());
+        app.groups().createGroup(new GroupData());
     }
 
     @Test
     public void canCreateGroupWithNameOnly() {
-        app.openGroupsPage();
-        var emptyGroup = new GroupData();
-        var groupWithName = emptyGroup.withName("pokemon");
-        app.createGroup(groupWithName);
+        app.groups().createGroup(new GroupData().withName("pokemon"));
     }
 
 }
