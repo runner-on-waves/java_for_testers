dc879f7e62db3b798d8a3cd0013b0d2174ec564d
Iryna Yakovleva <runner.on.waves@gmail.com>
2024-09-21 16:56:13 +0300

    Лекция 3.5  Параметризация запсука тестов. Изменена версия selenium до selenium-java:4.24.0 для корректного запуска тестов в chrome

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ApplicationManager {
@@ -11,9 +12,15 @@ public class ApplicationManager {
     private LoginHelper session;
     private GroupHelper groups;
 
-    public void init() {
+    public void init(String browser) {
         if (driver == null) {
+        if ("firefox".equals(browser)){
             driver = new FirefoxDriver();
+        } else if("chrome".equals(browser)) {
+            driver = new ChromeDriver();
+        } else {
+            throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+        }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
             driver.get("http://localhost/addressbook/");
             driver.manage().window().setSize(new Dimension(1075, 824));
