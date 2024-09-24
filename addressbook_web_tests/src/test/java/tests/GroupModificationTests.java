d41f451925704319db963bb83f6786c3ca5dbb4d
Iryna Yakovleva <runner.on.waves@gmail.com>
2024-09-24 06:28:48 +0300

    Лекция 3.6 Добавлены классы HelperBase и тест GroupModificationTests

---
+++ addressbook_web_tests/src/test/java/tests/GroupModificationTests.java
@@ -0,0 +1,14 @@
+package tests;
+
+import model.GroupData;
+import org.junit.jupiter.api.Test;
+
+public class GroupModificationTests extends TestBase{
+    @Test
+    void canModifyGroup(){
+        if (!app.groups().isGroupPresent()) {
+            app.groups().createGroup(new GroupData("new name", "new header", "new footer"));
+        }
+        app.groups().modifyGroup(new GroupData().withName("modified name"));
+    }
+}
