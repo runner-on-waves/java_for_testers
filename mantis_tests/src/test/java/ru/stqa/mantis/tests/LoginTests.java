857592aada90035da48103a484fe75ec5ec714ee
Iryna Yakovleva <runner.on.waves@gmail.com>
2024-10-26 18:38:55 +0300

    Задание 17. Добавлен модуль mantis_tests

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/LoginTests.java
@@ -0,0 +1,12 @@
+package ru.stqa.mantis.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LoginTests extends TestBase{
+    @Test
+    public void canLogin(){
+        app.session().login("administrator", "root");
+        Assertions.assertTrue(app.session().isLoggedIn());
+    }
+}
