4365512d8ea91fce8e92cf6236ef114fbd30aae4
Iryna Yakovleva <runner.on.waves@gmail.com>
2024-10-27 06:16:21 +0300

    Лекция 8.3 Взаимодействие с сервером по http. Добавлен класс httpSessionHelper

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -12,6 +12,7 @@ public class ApplicationManager {
     private String browser;
     private Properties properties;
     private SessionHelper sessionHelper;
+    private HttpSessionHelper httpSessionHelper;
 
     public void init(String browser, Properties properties) {
         this.browser = browser;
@@ -42,4 +43,14 @@ public class ApplicationManager {
         return sessionHelper;
     }
 
+    public HttpSessionHelper http() {
+        if(httpSessionHelper == null){
+            httpSessionHelper = new HttpSessionHelper(this);
+        }
+        return httpSessionHelper;
+    }
+
+    public String property(String name){
+        return properties.getProperty(name);
+    }
 }
--- mantis_tests/src/test/java/ru/stqa/mantis/tests/LoginTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/LoginTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class LoginTests extends TestBase{
     @Test
     public void canLogin(){
-        app.session().login("administrator", "root");
-        Assertions.assertTrue(app.session().isLoggedIn());
+        app.http().login("administrator", "root");
+        Assertions.assertTrue(app.http().isLoggedIn());
     }
 }
