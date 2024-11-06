4734841d36b0c05a382e024981e13a50afe56765
Iryna Yakovleva <runner.on.waves@gmail.com>
2024-11-06 07:42:03 +0300

    Лекция 9.4. Почтовый сервис DeveloperMail.com: получение почты

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/RegistrationHelper.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/RegistrationHelper.java
@@ -8,13 +8,29 @@ public class RegistrationHelper extends HelperBase {
         super(manager);
 
     }
+
     public void register(String user, String email) {
         click(By.xpath("//a[@href='signup_page.php']"));
         type(By.name("username"), user);
         type(By.name("email"), email);
         click(By.cssSelector("input[type = 'submit']"));
     }
-    public void confirmRegistration(String url,String user, String password) {
+
+    public void createUser(String user, String email) {
+        type(By.name("username"), manager.property("web.username"));
+        click(By.cssSelector("input[type = 'submit']"));
+        type(By.name("password"), manager.property("web.password"));
+        click(By.cssSelector("input[type = 'submit']"));
+        click(By.cssSelector("i[class = 'fa fa-gears menu-icon']"));
+        click(By.xpath("//a[@href='/mantisbt-2.27.0/manage_user_page.php']"));
+        click(By.xpath("//a[@href='manage_user_create_page.php']"));
+        type(By.name("username"), user);
+        type(By.name("realname"), user);
+        type(By.name("email"), email);
+        click(By.cssSelector("input[type = 'submit']"));
+    }
+
+    public void confirmRegistration(String url, String user, String password) {
         manager.driver().get(url);
         type(By.name("realname"), user);
         type(By.name("password"), password);
